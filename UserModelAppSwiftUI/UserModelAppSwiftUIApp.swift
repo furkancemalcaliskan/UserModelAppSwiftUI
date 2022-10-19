ea418dd24df7c25162c11af7836e42f462688faf
Furkan Cemal Caliskan <furkanc.caliskan@icloud.com>
2022-10-19 16:49:59 +0300

    Initial Commit

---
+++ UserModelAppSwiftUI/UserModelAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserModelAppSwiftUIApp.swift
+//  UserModelAppSwiftUI
+//
+//  Created by Furkan Cemal Çalışkan on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct UserModelAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
